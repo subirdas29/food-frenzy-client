fdc25cd7890d1a6580fe24d435354128f6e3610f
subirdas29 <subirdas1045@gmail.com>
2024-08-13 21:12:16 +0600

    all added

--- eslint.config.js
+++ eslint.config.js
@@ -16,6 +16,9 @@ export default [
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
       },
+      "env": {
+    "node": true
+  },
     },
     settings: { react: { version: '18.3' } },
     plugins: {
