46e25bb78c89fa6ae71b0a4aacd6a893e797b76f
subirdas29 <subirdas1045@gmail.com>
2024-08-31 01:04:52 +0600

    swiper added

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -1,8 +1,12 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
-
+import slide1 from '../../../assets/Home/Category/slide1.jpg'
+import slide2 from '../../../assets/Home/Category/slide2.jpg'
+import slide3 from '../../../assets/Home/Category/slide3.jpg'
+import slide4 from '../../../assets/Home/Category/slide4.jpg'
+import slide5 from '../../../assets/Home/Category/slide5.jpg'
 
 // Import Swiper styles
 import 'swiper/css';
@@ -14,26 +18,32 @@ import { Pagination } from 'swiper/modules';
 
 const Category = () => {
   return (
-    <Swiper
+   <div className='mx-48 my-20'>
+     <Swiper
     slidesPerView={4}
     spaceBetween={30}
-    centeredSlides={true}
     pagination={{
       clickable: true,
     }}
     modules={[Pagination]}
     className="mySwiper"
   >
-    <SwiperSlide>Slide 1</SwiperSlide>
-    <SwiperSlide>Slide 2</SwiperSlide>
-    <SwiperSlide>Slide 3</SwiperSlide>
-    <SwiperSlide>Slide 4</SwiperSlide>
-    <SwiperSlide>Slide 5</SwiperSlide>
-    <SwiperSlide>Slide 6</SwiperSlide>
-    <SwiperSlide>Slide 7</SwiperSlide>
-    <SwiperSlide>Slide 8</SwiperSlide>
-    <SwiperSlide>Slide 9</SwiperSlide>
+    <SwiperSlide><img src={slide1} alt="" />
+    <h1 className='uppercase text-3xl text-center -mt-20 text-white shadow-black shadow-md'>Salads</h1>
+    </SwiperSlide>
+    <SwiperSlide><img src={slide2} alt="" />
+    <h1 className='uppercase text-3xl text-center -mt-20 text-white shadow-black shadow-md'>Soups</h1></SwiperSlide>
+    <SwiperSlide><img src={slide3} alt="" />
+    <h1 className='uppercase text-3xl text-center -mt-20 text-white shadow-black shadow-md'>Pizzas</h1></SwiperSlide>
+    <SwiperSlide><img src={slide4} alt="" />
+    <h1 className='uppercase text-3xl text-center -mt-20 text-white shadow-black shadow-md'>Desserts</h1>
+    </SwiperSlide>
+    <SwiperSlide><img src={slide5} alt="" />
+    <h1 className='uppercase text-3xl text-center -mt-16 text-white shadow-black shadow-md'>Salads</h1>
+    </SwiperSlide>
+    
   </Swiper>
+   </div>
   )
 }
 
